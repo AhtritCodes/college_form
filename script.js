b784e569920ca03dc0064d934162e54eaf9d7318
AhtritCodes <tirtha13july2002sonofmp@gmail.com>
2023-04-04 23:13:31 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnSuccess = document.getElementById ('btn-success');
+var userName;
+var userMail;
+var pass;
+var userdob;
+
+
+// console.log(btnSuccess)
+// console.log(input.user);
+btnSuccess.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('button clicked');
+    userName = document.getElementById('user').value;
+    userMail = document.getElementById('usermail').value;
+    pass = document.getElementById('pass').value;
+    userdob = document.getElementById('userdob').value;
+
+    console.log(userName + ' ' + userMail + ' ' + pass + ' ' + userdob);
+
+    // window.location.href="";
+
+    document.getElementById('name').innerText = userName;
+    document.getElementById('email').innerText = userMail;
+    document.getElementById('dob').innerText = userdob;
+
+})
